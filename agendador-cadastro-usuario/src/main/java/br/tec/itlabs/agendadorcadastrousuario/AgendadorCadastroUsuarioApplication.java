24bdccafa0a6c1118d4347e0cfaba885431cef8d
Ygor Nascimento <ygo.nascimento@outlook.com>
2025-08-02 07:21:05 -0300

    First Commit - Spring Startter

---
+++ agendador-cadastro-usuario/src/main/java/br/tec/itlabs/agendadorcadastrousuario/AgendadorCadastroUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.tec.itlabs.agendadorcadastrousuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorCadastroUsuarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendadorCadastroUsuarioApplication.class, args);
+    }
+
+}
---
+++ agendador-cadastro-usuario/src/test/java/br/tec/itlabs/agendadorcadastrousuario/AgendadorCadastroUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.tec.itlabs.agendadorcadastrousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorCadastroUsuarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
