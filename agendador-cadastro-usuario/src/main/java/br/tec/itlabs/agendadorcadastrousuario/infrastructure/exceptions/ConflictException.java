0dc64d4586a0dc08896cc0faf2fdbb3cc81afae8
Ygor Nascimento <ygo.nascimento@outlook.com>
2025-08-04 07:40:28 -0300

    Testando método POST e salvando usuário no banco de dados.

---
+++ agendador-cadastro-usuario/src/main/java/br/tec/itlabs/agendadorcadastrousuario/infrastructure/exceptions/ConflictException.java
@@ -0,0 +1,13 @@
+package br.tec.itlabs.agendadorcadastrousuario.infrastructure.exceptions;
+
+public class ConflictException extends RuntimeException{
+
+    public ConflictException(String mensagem){
+        super(mensagem);
+    }
+
+    public ConflictException(String mensagem, Throwable throwable){
+        super(mensagem);
+    }
+
+}
